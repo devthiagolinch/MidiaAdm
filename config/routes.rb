78f2fb4c0ee0ff1b1bbab4e20d77156a95e75f47
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-03-05 10:38:38 -0400

    Gemfile | Devise and Devise Views: Admins and Users | Rails task dev:setup | Controllers and Heritage: Admins and Users ; AdminsBakcoffice and UsersBackoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
