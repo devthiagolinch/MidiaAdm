6114cb1c1821f362bdfbcf4094871bc6968e7cbb
Thiago Linch <thglinchin18@gmail.com>
2021-03-09 15:09:36 -0400

    Front End Users: Page Resumo

--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -1,4 +1,36 @@
 class UsersBackoffice::WelcomeController < UsersBackofficeController
-  def index
-  end
+  before_action :set_user
+    before_action :verify_password, only: [:update]
+
+    def edit
+        @user.build_user_profile if @user.user_profile.blank?
+    end
+
+    def update
+        if @user.update(params_user)
+            bypass_sign_in(@user) # no update
+            redirect_to users_backoffice_welcome_index_path	, notice: "Usuário
+            atualizado com sucesso!"
+        else
+            render :index
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find(current_user.id)
+    end
+
+    def params_user
+        params.require(:user).permit(:first_name, :last_name, :email,
+        :password, :password_confirmation, user_profile_attributes: [:id,:address, :cellphone, :birthdate])
+    end
+        
+    def verify_password
+        if params[:user][:password].blank? &&
+        params[:user][:password_confirmation].blank?
+        params[:user].extract!(:password, :password_confirmation)
+        end
+    end
 end
