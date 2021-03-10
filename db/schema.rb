50290ff16c230c1f8e3036f5f5dcd8df8a12faa6
Thiago Linch <thglinchin18@gmail.com>
2021-03-10 17:25:00 -0400

    schema with schedule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_124454) do
+ActiveRecord::Schema.define(version: 2021_03_10_190946) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2021_03_09_124454) do
     t.string "last_name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.date "day"
+    t.string "morning_tranmission"
+    t.string "morning_files"
+    t.string "night_transmission"
+    t.string "night_files"
+    t.string "night_sound"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_profiles", force: :cascade do |t|
