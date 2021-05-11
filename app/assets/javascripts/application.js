77fc62c4f19f0e7011d30d7d8ff0a2b3f033d335
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-05-11 08:50:10 -0400

    Require fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require_tree .
 //= require jquery/dist/jquery
 //= require popper.js/dist/popper
 //= require bootstrap/dist/js/bootstrap.bundle
