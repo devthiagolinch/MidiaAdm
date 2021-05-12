edb2dcf092f91ce40383b965909d5abddd43ca5d
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-05-12 00:28:46 -0400

    Removing web-console

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,5 @@ module MidiaAdm
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Allow Web Console from Vagrant
-    config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
