b88d422ef939ab1018370b1e08aebdf4bd784bca
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-05-11 22:29:07 -0400

    Assets fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,8 @@
 //= require popper.js/dist/popper
 //= require bootstrap/dist/js/bootstrap.bundle
 //= require jquery.easing/jquery.easing
-//= require @fortawesome/fontawesome-free/js/all.js
+//= require chart.js/dist/Chart.min
+//= require @fortawesome/fontawesome-free/js/all.min
 //= require datatables.net-bs4/js/dataTables.bootstrap4
 //= require datatables.net/js/jquery.dataTables
 //= require sb-admin-2
-//= require chart.js/dist/Chart.min
