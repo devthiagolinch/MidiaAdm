2ca3d817355c9edbf5c548d03a4e59239fd600b6
Thiago Linchin <dev.thiagolinch@gmail.com>
2021-05-14 10:42:19 -0400

    Fix Glyphicons#02

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module MidiaAdm
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Bootstrap Glyphicons Fix
+    config.assets.paths << Rails.root.join('node_modules','bootstrap','fonts')
   end
 end
